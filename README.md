e1731d7968df9a77a2c8adf08254a41f9a6f547e
Gabriel Fantin <fantinee@live.com>
2026-08-04 23:39:06 -0300

    Códigos de entrada.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Loja
 Criando e evoluindo
+A ideia é ir criando e estudando!
+Conforme for aprendendo vou evoluindo.
---
+++ inicio/base.java
@@ -0,0 +1,17 @@
+package cadastro;
+
+import java.util.Scanner;
+
+public class cadastro {
+		public static void main(String[]args) {
+			System.out.println("Bem Vindo!");
+			
+			 Scanner entrada = new Scanner(System.in);
+			 	
+			 int nomeCliente;
+			 
+			 System.out.println("Qual o seu nome?");
+			 nomeCliente = entrada.nextInt();
+			 
+		}
+}
